b44927feb1b64276dba75d48f7cdf7e6207242c9
kacychen <kacychen@tencent.com>
2026-04-01 16:56:15 +0800

    fix: login云函数改回wx-server-sdk获取OPENID

--- miniprogram/cloudfunctions/login/index.js
+++ miniprogram/cloudfunctions/login/index.js
@@ -1,3 +1,7 @@
-exports.main = async (event, context) => {
-  return { openid: context.WXCONTEXT.OPENID };
+const cloud = require('wx-server-sdk');
+cloud.init({ env: cloud.DYNAMIC_CURRENT_ENV });
+
+exports.main = async () => {
+  const { OPENID } = cloud.getWXContext();
+  return { openid: OPENID };
 };
