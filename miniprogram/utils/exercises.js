17722220e64f5ff6c4d6d529103f9b8547b94eda
kacychen <kacychen@tencent.com>
2026-04-10 13:16:34 +0800

    feat: add exercises library and register exercise-search route

---
+++ miniprogram/utils/exercises.js
@@ -0,0 +1,17 @@
+// miniprogram/utils/exercises.js
+module.exports = [
+  { type: 'aerobic',  type_name: '有氧运动', name: '跑步',   met: 8.0 },
+  { type: 'aerobic',  type_name: '有氧运动', name: '快走',   met: 4.5 },
+  { type: 'aerobic',  type_name: '有氧运动', name: '骑行',   met: 6.0 },
+  { type: 'aerobic',  type_name: '有氧运动', name: '游泳',   met: 7.0 },
+  { type: 'aerobic',  type_name: '有氧运动', name: '跳绳',   met: 10.0 },
+  { type: 'strength', type_name: '力量训练', name: '哑铃',   met: 5.0 },
+  { type: 'strength', type_name: '力量训练', name: '深蹲',   met: 5.5 },
+  { type: 'strength', type_name: '力量训练', name: '俯卧撑', met: 5.0 },
+  { type: 'ball',     type_name: '球类运动', name: '篮球',   met: 7.0 },
+  { type: 'ball',     type_name: '球类运动', name: '乒乓球', met: 4.5 },
+  { type: 'ball',     type_name: '球类运动', name: '羽毛球', met: 5.5 },
+  { type: 'other',    type_name: '其他',     name: '瑜伽',   met: 3.0 },
+  { type: 'other',    type_name: '其他',     name: '舞蹈',   met: 4.5 },
+  { type: 'other',    type_name: '其他',     name: '爬山',   met: 6.5 },
+];
